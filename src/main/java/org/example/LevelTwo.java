6fe5370c5b84f5888a6f9138ff28add16ece1e7e
Shalev <shalev213@gmail.com>
2024-09-25 16:37:21 +0300

    mission 2: add 2 buttons

--- src/main/java/org/example/LevelTwo.java
+++ src/main/java/org/example/LevelTwo.java
@@ -1,32 +1,66 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class LevelTwo extends AbstractLevel implements KeyListener {
+    private final JButton riddleButton;
+    private final JButton fakeRiddleButton;
     private ImageIcon spaceshipBackground;
     private Astronaut astronaut;
     private int xOfBackground = 0;
     private boolean rightPressed = false;
     private boolean leftPressed = false;
+    private int fakeButtonWidth = 200;
 
 
     public LevelTwo(int width, int height) {
+        // קריאה לבנאי של המחלקה האבסטרקטית אחרי אתחול הרקעים
+        super.windowWidth = width;
+        super.windowHeight = height;
         // אתחול הרקעים קודם לקריאה לבנאי של המחלקה האבסטרקטית
         this.spaceBackgroundOne = null;
         this.spaceBackgroundTwo = null;
         this.spaceshipBackground = new ImageIcon("src/main/java/resources/levelTwoBackground.png");
         this.xOfBackground = -(this.spaceshipBackground.getIconWidth() - this.windowWidth) / 2;
 
+        this.riddleButton = new JButton("?");
+        this.riddleButton.setFont(new Font("Arial", Font.BOLD, 25));
+        this.riddleButton.setContentAreaFilled(false);
+        this.riddleButton.setBorderPainted(false);
+        this.riddleButton.setFocusPainted(false);
+        this.riddleButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        this.riddleButton.setFocusable(false);
+        this.riddleButton.setForeground(new Color(142, 109, 4, 224));
+
+        this.riddleButton.addActionListener(e -> {
+            System.out.println("you clicked on: showRiddleButton");
+        });
+
+        this.add(riddleButton);
+
+        this.fakeRiddleButton = new JButton("click here");
+        this.fakeRiddleButton.setFont(new Font("Arial", Font.BOLD, 30));
+        this.fakeRiddleButton.setContentAreaFilled(false);
+        this.fakeRiddleButton.setBorderPainted(false);
+        this.fakeRiddleButton.setFocusPainted(false);
+//        this.fakeRiddleButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        this.fakeRiddleButton.setFocusable(false);
+        this.fakeRiddleButton.setForeground(new Color(60, 124, 144, 255));
+
+        this.fakeRiddleButton.addActionListener(e -> {
+            System.out.println("hahahaha you clicked on: fakeRiddleButton");
+        });
+
+        this.add(fakeRiddleButton);
+
+
         this.astronaut = new Astronaut();
         this.astronaut.setX((int) ((windowWidth - this.astronaut.getWidth()) / 2));
         this.astronaut.setY(((windowHeight - this.astronaut.getHeight()) / 2) + 60);
-
-        // קריאה לבנאי של המחלקה האבסטרקטית אחרי אתחול הרקעים
-        super.windowWidth = width;
-        super.windowHeight = height;
 
 
         this.setFocusable(true);
@@ -39,7 +73,10 @@ public class LevelTwo extends AbstractLevel implements KeyListener {
         super.paintComponent(graphics);
         if (spaceshipBackground != null) {
             this.spaceshipBackground.paintIcon(null, graphics, xOfBackground, 0);
+            this.riddleButton.setBounds(xOfBackground + 1320,140,28,45);
+            this.fakeRiddleButton.setBounds(xOfBackground + (spaceshipBackground.getIconWidth() - this.fakeButtonWidth) / 2,260,this.fakeButtonWidth,45);
         }
+
         astronaut.paintAstronaut(graphics);
     }
 
