a72bff0ec4ea55222363577b1228747906868e96
אוריאל <urielschool123@gmail.com>
2024-10-20 17:45:54 +0300

    ++

--- src/main/java/org/example/LobbyPanel.java
+++ src/main/java/org/example/LobbyPanel.java
@@ -47,44 +47,31 @@ public class LobbyPanel extends JPanel {
         this.exitButton.setForeground(new Color(31, 191, 230));
         this.exitButton.setFocusPainted(false);
         this.exitButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
-//        this.exitButton.setFocusPainted(false);  // לא לצבוע את המסגרת כשהכפתור מקבל פוקוס
-//        this.exitButton.setOpaque(false);  // להפוך את הכפתור לשקוף
-        this.exitButton.setContentAreaFilled(false);  // להסיר את מילוי האזור של הכפתור
-//        this.exitButton.setBorderPainted(false);  // לא לצייר את הגבול של הכפתור
+        this.exitButton.setContentAreaFilled(false);
         this.exitButton.setBorder(BorderFactory.createLineBorder(new Color(31, 191, 230), 10));  // גבול בעובי 2 פיקסלים
-
-
 
         this.add(exitButton);
 
 
-//        this.levelButton1 = new LevelButton(100, 520, "1");  // כפתור ראשון
-//        this.levelButton2 = new LevelButton(300, 520, "2");  // כפתור שני
-//        this.levelButton3 = new LevelButton(520, 520, "3");  // כפתור שלישי
-//        this.levelButton4 = new LevelButton(720, 520, "4");  // כפתור רביעי
-//        this.levelButton5 = new LevelButton(920, 520, "5");  // כפתור חמישי
 
 
-        this.levelButton1 = new LevelButton(80, 520);  // כפתור ראשון
-        this.levelButton2 = new LevelButton(275, 520);  // כפתור שני
-        this.levelButton3 = new LevelButton(480, 520);  // כפתור שלישי
-        this.levelButton4 = new LevelButton(680, 520);  // כפתור רביעי
-        this.levelButton5 = new LevelButton(880, 520);  // כפתור חמישי
-
-
-//        this.levelButton1 = new LevelButton(levelButtonsX, levelButtonsY, "1");
+        this.levelButton1 = new LevelButton(80, 520);
         this.levelButton1.setEnabled(true);
         this.add(levelButton1);
-//        this.levelButton2 = new LevelButton(levelButtonsX + space + levelButton1.getWidth(), levelButtonsY, "2");
-        this.levelButton2.setEnabled(true);//*** צריך להיות false
+
+        this.levelButton2 = new LevelButton(275, 520);
+        this.levelButton2.setEnabled(true);
         this.add(levelButton2);
-//        this.levelButton3 = new LevelButton(levelButtonsX + 2 * (space + levelButton1.getWidth()), levelButtonsY, "3");
+
+        this.levelButton3 = new LevelButton(480, 520);
         this.levelButton3.setEnabled(true);//***
         this.add(levelButton3);
-//        this.levelButton4 = new LevelButton(levelButtonsX + 3 * (space + levelButton1.getWidth()), levelButtonsY, "4");
-        this.levelButton4.setEnabled(true);//***
+
+        this.levelButton4 = new LevelButton(680, 520);
+        this.levelButton4.setEnabled(true);
         this.add(levelButton4);
-//        this.levelButton5 = new LevelButton(levelButtonsX + 4 * (space + levelButton1.getWidth()), levelButtonsY, "5");
+
+        this.levelButton5 = new LevelButton(880, 520);
         this.add(levelButton5);
     }
     public void paintComponent(Graphics graphics) {
