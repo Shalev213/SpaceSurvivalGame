9fd05a54335f0f30bd87528bc2e500de84d05232
Shalev <shalev213@gmail.com>
2025-01-20 18:39:34 +0200

    Instructions panel

--- src/main/java/org/example/LevelInstructions.java
+++ src/main/java/org/example/LevelInstructions.java
@@ -9,16 +9,29 @@ public class LevelInstructions extends JPanel {
     private String strBody;
     private String path;
     private ImageIcon screenshot;
+    private ImageIcon background;
     private int imageX;
     private int imageY;
+    private int imageWidth = 550;
+    private int imageHeight = 375;
     private JButton startGameButton;
     private int currentLevel;
     private boolean isClicked = false;
+    private int yTitle = 30;
+    private int xTitle;
+    private int titleWidth = 450;
+    private int titleHeight = 60;
+    private int xBody;
+    private int bodyWidth = 800;
+    private int space = 90;
+
 
 //    private InstructionsButton instructionsButton;
 
 
     public LevelInstructions(int currentLevel, int windowWidth, int windowHeight) {//int windowWidth, int windowHeight, String title, String textBody, String path, int imageX, int imageY
+        this.background = new ImageIcon("src/main/java/resources/instructionBackgrond.png");
+
         this.currentLevel = currentLevel;
         this.setSize(windowWidth, windowHeight);
         this.setLayout(null);
@@ -27,22 +40,27 @@ public class LevelInstructions extends JPanel {
 
         setPanelByLevel();
 
+        this.xTitle = (windowWidth - titleWidth) / 2;
+
         this.title = new JLabel("Level " + this.currentLevel + " - Instructions");
         this.title.setForeground(new Color(31, 191, 230));
         this.title.setFont(new Font("Arial", Font.BOLD, 45));
-        this.title.setBounds(275, 60, 600, 100);
+        this.title.setBounds(this.xTitle, this.yTitle, this.titleWidth, this.titleHeight);
         this.add(this.title);
+
+        this.xBody = (windowWidth - this.bodyWidth) / 2;
 
         this.bodyLabel = new JLabel();
         this.bodyLabel.setText(strBody);
         this.bodyLabel.setForeground(new Color(31, 191, 230));
-        this.bodyLabel.setFont(new Font("Arial", Font.BOLD, 45));
-        this.bodyLabel.setBounds(10, 200, 550, 800);
+        this.bodyLabel.setFont(new Font("SansSerif", Font.PLAIN, 25));
+        this.bodyLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        this.bodyLabel.setBounds(this.xBody, this.yTitle + this.space, this.bodyWidth, 190);
         this.add(this.bodyLabel);
 
         this.screenshot = new ImageIcon(path);
-        this.imageX = 100;
-        this.imageY = 400;
+        this.imageX = (windowWidth - imageWidth) / 2;
+        this.imageY = 320;
 
 //        this.startGameButton = new JButton("Start");
 //        this.startGameButton.setFont(new Font("Arial", Font.BOLD, 25));
@@ -60,10 +78,18 @@ public class LevelInstructions extends JPanel {
 
     public void paintComponent(Graphics graphics) {
 //        super.paint(graphics);
-        if (this.screenshot != null) {
-            graphics.drawImage(this.screenshot.getImage(), this.imageX, this.imageY, 550, 375, null);
-//            this.screenshot.paintIcon(null, graphics, this.imageX, this.imageY);
+        if (this.background != null){
+            this.background.paintIcon(null, graphics, 0, 0);
         }
+        if (this.screenshot != null) {
+            graphics.drawImage(this.screenshot.getImage(), this.imageX, this.imageY, this.imageWidth, this.imageHeight, null);
+
+            Graphics2D g2d = (Graphics2D) graphics;
+            g2d.setColor(Color.BLACK);
+            g2d.setStroke(new BasicStroke(10));
+            g2d.drawRect(this.imageX, this.imageY, this.imageWidth, this.imageHeight);
+        }
+
     }
 
 
@@ -88,32 +114,10 @@ public class LevelInstructions extends JPanel {
             }
             case 5 -> {
                 path = "src/main/java/resources/level5instructions.png";
-                strBody = "5555555555555555555555555555555";
+                strBody = "Messi the goat of level 5";
             }
         }
     }
-
-
-//    public String levelTwoBody() {
-//        String sade = "adasd";
-//        return sade;
-//    }
-//
-//    public String levelThreeBody() {
-//        String sade = "adasd";
-//        return sade;
-//    }
-//
-//    public String levelFourBody() {
-//        String sade = "adasd";
-//        return sade;
-//    }
-//
-//    public String levelFiveBody() {
-//        String sade = "adasd";
-//        return sade;
-//    }
-
 
     public boolean isClicked() {
         return isClicked;
